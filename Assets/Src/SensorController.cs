69d088878a51b8ebe05103e3b9dd13683b97e169
PentagramPro <realevilhq@gmail.com>
2017-12-27 22:56:47 +0300

    Sensor

--- Assets/Src/SensorController.cs
+++ Assets/Src/SensorController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SensorController : MonoBehaviour {
 
     public float Signal { get; private set; }
+	public float MaxSignal = 1;
+	public float MaxDistance = 10;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,17 +17,31 @@ public class SensorController : MonoBehaviour {
 		
 	}
 
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        var sensorTarget = collision.gameObject.GetComponent<SensorTargetController>();
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		var sensorTarget = collision.gameObject.GetComponent<SensorTargetController>();
 
-        if(sensorTarget == null)
-        {
-            return;
-        }
+		if (sensorTarget == null)
+		{
+			return;
+		}
 
-        Signal = (transform.position - sensorTarget.transform.position).magnitude;
-    }
+		float distance = (transform.position - sensorTarget.transform.position).magnitude;
+		distance = Mathf.Clamp(distance, 0, MaxDistance);
+		Signal = 1 - distance / MaxDistance;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		Signal = 0;
+	}
+
+
 
 
 }
---
+++ Assets/Src/SensorMonitorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SensorMonitorController : MonoBehaviour {
+
+	public SensorController Sensor;
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = string.Format("{0}", Sensor.Signal);
+	}
+}
