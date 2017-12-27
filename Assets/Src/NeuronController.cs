ad57fc0b65e19b4a4d8382cf2ee8aaad456da39d
PentagramPro <realevilhq@gmail.com>
2017-12-28 00:54:11 +0300

    hooked up network to animal

--- Assets/Src/NeuronController.cs
+++ Assets/Src/NeuronController.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 using AForge.Neuro;
 
 public class NeuronController : MonoBehaviour {
-
-    ActivationNetwork network = new ActivationNetwork(new SigmoidFunction(2),2,2,1);
+	public List<SensorController> Sensors;
+	public List<ActuatorController> Actuators;
+	ActivationNetwork network = null;
 	// Use this for initialization
 	void Start () {
+		network = new ActivationNetwork(new SigmoidFunction(2), Sensors.Count, Actuators.Count, 1);
 		network.Randomize();
 		
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate() {
+		double[] sensors = Sensors.ConvertAll<double>((s) => { return s.Signal; }).ToArray();
+		network.Compute(sensors);
+		int i = 0;
+		foreach(var a in Actuators)
+		{
+			a.SetForce((float)network.Output[i]);
+			i++;
+		}
 	}
 }
