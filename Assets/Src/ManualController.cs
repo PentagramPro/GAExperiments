e51abe5a08ba9895aaa7d8b3580fa466dc7bbe0f
mm@redmadrobot.com <mm@redmadrobot.com>
2017-12-27 03:09:34 +0300

    .

---
+++ Assets/Src/ManualController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualController : MonoBehaviour {
+
+    public float Speed = 3;
+    public float RotationSpeed = 2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float velocity = 0;
+        float rotation = 0;
+		if(Input.GetKey(KeyCode.UpArrow))
+        {
+            velocity = Speed * Time.smoothDeltaTime;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity =- Speed * Time.smoothDeltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotation += RotationSpeed * Time.smoothDeltaTime;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotation -= RotationSpeed * Time.smoothDeltaTime;
+        }
+
+        transform.Rotate(Vector3.forward, rotation);
+        Debug.Log(transform.right);
+        transform.position += transform.right * velocity;
+
+    }
+}
---
+++ Assets/Src/SensorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorController : MonoBehaviour {
+
+    public float Signal { get; private set; }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        var sensorTarget = collision.gameObject.GetComponent<SensorTargetController>();
+
+        if(sensorTarget == null)
+        {
+            return;
+        }
+
+        Signal = (transform.position - sensorTarget.transform.position).magnitude;
+    }
+
+
+}
