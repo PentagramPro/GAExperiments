355f9d27bd68dc45234bdee07a1b6106c943e365
PentagramPro <realevilhq@gmail.com>
2017-12-27 17:53:50 +0300

    AForge libraries

---
+++ Assets/Src/NeuronController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AForge.Neuro;
+
+public class NeuronController : MonoBehaviour {
+
+    ActivationNetwork network = new ActivationNetwork(new SigmoidFunction(2),2,2,1);
+	// Use this for initialization
+	void Start () {
+		network.Randomize();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
